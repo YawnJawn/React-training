5f69645f865c2ff7beea149444dcf417ea85f8b4
Tin <ttran79@rrc.ca>
2025-05-08 13:37:13 -0500

    Adding event listeners to text area onChange

--- vite-project/src/components/PostsList.jsx
+++ vite-project/src/components/PostsList.jsx
@@ -1,12 +1,17 @@
 import Post from './Post';
+import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 
 function PostsList(){
     return (
+        <>
+        <NewPost />
         <ul className={classes.posts}>
             <li><Post author="Tin" body="Learning"/></li>
             <li><Post author="Student" body="Hello"/></li>
         </ul>
+        </>
+
     );
 }
 
