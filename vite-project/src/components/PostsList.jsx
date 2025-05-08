4f33b8b6e3abca36388247b5bc34968977d2c2e4
Tin <ttran79@rrc.ca>
2025-05-08 15:15:28 -0500

    Lifting up state

--- vite-project/src/components/PostsList.jsx
+++ vite-project/src/components/PostsList.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+
 import Post from './Post';
 import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 
+
 function PostsList(){
+    const [enteredBody, setCurrentEnteredBody] = useState('');
+    const [enteredAuthor, setCurrentEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event){
+        setCurrentEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event){
+        setCurrentEnteredAuthor(event.target.value);
+    }
+
+
     return (
         <>
-        <NewPost />
+        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
         <ul className={classes.posts}>
+            <li><Post author={enteredAuthor} body={enteredBody}/></li>
             <li><Post author="Tin" body="Learning"/></li>
             <li><Post author="Student" body="Hello"/></li>
         </ul>
