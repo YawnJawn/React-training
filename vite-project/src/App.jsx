e18f87eca61fcb3d68137ffb1e7c9776bbc8cd89
Tin <ttran79@rrc.ca>
2025-05-08 10:35:01 -0500

    Adding props and reusing props with different values

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,12 @@
 import Post from './components/Post';
 
 function App(){
-  return <Post />;
+  return (
+  <main>
+    <Post author="Tin" body="Learning"/>
+    <Post author="Student" body="Hello"/>
+  </main>
+  );
 }
 
 export default App;
--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -1,12 +1,9 @@
-const names = ['Tin', 'Tran'];
-
-function Post() {
-    const chosenName = Math.random() > 0.5 ? names[0] : names [1];
+function Post(props) {
 
     return (
     <div>
-        <p>{chosenName}</p>
-        <p>Learning React.js!</p>
+        <p>{props.author}</p>
+        <p>{props.body}</p>
     </div>
     );
 }
